f42334c4ee4cccf379336705eaf480bfb2515878
Twoguini <twoguini@gmail.com>
2025-12-02 16:21:16 -0300

    started logic

--- dayOne/partOne.c
+++ dayOne/partOne.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+#define MIN 0
+#define MAX 99
+#define INITIAL_POSITION 50
+
+// LEFT  L -> Lower  Numbers - (Subtract) 
+// RIGHT R -> Righer Numbers + (ADD) 
+
+// LEFT  from 0  goes to Finish, Ex.: From 0  L1 -> 99
+// RIGHT from 99 starts over   , Ex.: From 99 R1 -> 0
+
 int main() {
     
+    FILE *input = fopen("../inputs/dayOne.txt", "r");
+    if (!input) {
+        printf("File Do Not Exists");
+        return 1;
+    }
+
+    char currentStr[256];
+    fgets(currentStr, sizeof(currentStr), input);
+    
+    printf("%s", currentStr);
+    
+    fclose(input);
+    return 0;
 }
