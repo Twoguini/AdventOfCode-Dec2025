b43b1b809a159d9f6cda49fad11868293b3d1593
Twoguini <twoguini@gmail.com>
2025-12-09 14:23:29 -0300

    possible int mem problem

--- dayTwo/partOne.c
+++ dayTwo/partOne.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INPUT_FILE "../inputs/dayTwoTest.txt"
+#define INPUT_FILE "../inputs/dayTwo.txt"
 
 // INPUT
 /// in your input, they appear on a single long line.
@@ -18,14 +18,37 @@
 /// @return 1 for is made only of some sequence of digits repeated twice or 0 if that is not true
 int hasRepeatedSeq(char *number) {
 
+  int numberLen = strlen(number);
+  char *halfNumber = (number + strlen(number)/2);
+
+  // If nnumber length is not even, then it cannot repeat twice a sequence
+  if(numberLen % 2 != 0) return 0;
+
+  if(strncmp(number, halfNumber, numberLen/2) == 0) {
+    return 1;
+  }
+
+  return 0; 
 }
 
-/// @brief Returns the number of numbers that repeat the sequence of digits in the range
+/// @brief Checks and sums the sequence
 /// @param minRange 
 /// @param maxRange 
-/// @return Number of numbers that repeat the sequence of digits in the range
-int repeatedSequences(char *minRange, char *maxRange) {
+/// @return 0
+int repeatedSequences(char *minRange, char *maxRange, long long *totalSum) {
 
+  for(int i = atoi(minRange); i <= atoi(maxRange); i++) {
+    char compareC[256];
+    sprintf(compareC, "%d", i);
+
+    if(hasRepeatedSeq(compareC) == 1) {
+      *totalSum += i;
+    }
+  }
+
+  printf("TOTAL SUM: %lld\n", *totalSum);
+
+  return 0;
 }
 
 int main() {
@@ -55,21 +78,23 @@ int main() {
   char rangeMin[256];
   char rangeMax[256];
 
+  long long totalSum = 0;
+
   while (currentRange != NULL) {
     printf("CURRENT RANGE: %s\n", currentRange);
 
     InsiCurrentRange = strtok_r(currentRange, "-", &insiRangeDelimiter);
     while(InsiCurrentRange != NULL) {
       strcpy(rangeMin, InsiCurrentRange);
-      printf("MIN: %s - ", rangeMin);
       InsiCurrentRange = strtok_r(NULL, "-", &insiRangeDelimiter);
       strcpy(rangeMax, InsiCurrentRange);
-      printf("MAX: %s\n", rangeMax);
       InsiCurrentRange = strtok_r(NULL, "-", &insiRangeDelimiter);
+      repeatedSequences(rangeMin, rangeMax, &totalSum);
     }
 
     currentRange = strtok_r(NULL, ",", &rangeDelimiter);
   }
+  printf("MAX COMPARE: %lld\n", totalSum);
 
   fclose(input);
   free(inputLine);
